ee8a2d5357ab5062c6f0208aa31182e8f32bd9df
princess17-6124 <8angeleyes@gmail.com>
2026-02-26 18:56:16 -0800

    Oh my god!

--- gallery_script.js
+++ gallery_script.js
@@ -1,5 +1,21 @@
 const imageGallery = [
     "link",
     "link",
-    "link",
-]
+    "link"
+];
+const imageElement= document.getElementById('gallery-image');
+const prevButton = document.getElementById('prev-btn');
+const prevButton = document.getElementById('next-btn');
+let currentImageIndex = 0;
+function updateImage() {
+    imageElement.src = imageGallery[currentImageIndex];
+}
+updateImage();
+nextButton.addEventListener('click', function() {
+    currentImageIndex = (currentImageIndex + 1) % imageGallery.length;
+    updateImage();
+});
+prevButton.addEventListener('click', function() {
+    currentImageIndex = (currentImageIndex - 1) % imageGallery.length;
+    updateImage();
+});
